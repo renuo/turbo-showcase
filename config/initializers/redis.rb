dbb1a3d39292bb4dee7204ffec7b1d19420484ae
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-09 16:23:01 +0200

    Updated redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
+# rubocop:disable Style/GlobalVars
 $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# rubocop:enable Style/GlobalVars
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,3 +12,18 @@ Sidekiq.configure_client do |config|
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
+
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
